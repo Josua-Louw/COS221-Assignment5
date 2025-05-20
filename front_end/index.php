2d34122a0c25430aceff7e66a9be10fb35a3d656
antonyvs05 <vanstratenantony@gmail.com>
2025-05-20 14:33:38 +0200

    Branch for all front end development.

---
+++ front_end/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Home</title>
+
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <div class = "logo-container">
+        <!--Logo-->
+        <img src = "" alt = "Logo">
+    </div>
+    
+    <div class = "search-container">
+         <label>
+        <input type="text" placeholder="Search" id = "search-input" onkeyup = > <!--function to search for onkeyup-->
+    </label>
+    </div>
+   
+    <?php include 'footer.php'; ?>
+</body>
+</html>
