c70c4277a310cecf449ba165a02da32a2f48a810
Josua Louw <datalles19@gmail.com>
2025-05-23 13:54:10 +0200

    add api calls where I could

--- front_end/scripts/stores.js
+++ front_end/scripts/stores.js
@@ -14,4 +14,64 @@
             const storeType = document.querySelector('select').value;
             console.log('Searching for:', searchTerm, 'Type:', storeType);
         });
-  
+
+function getAllStores() {
+  const body = {
+    type: 'GetStores'
+  }
+
+  return sendRequest(body);
+}
+
+function followStore(apikey, storeID) {
+  const body = {
+    type: 'Follow',
+    apikey: apikey,
+    store_id: storeID
+  }
+
+  return sendRequest(body);
+}
+
+function getFollowingStores(apikey) {
+    const body = {
+        type: 'GetFollowing',
+        apikey: apikey
+    }
+
+    return sendRequest(body);
+}
+
+function unfollowStore(apikey) {
+    const body = {
+        type: 'GetFollowing',
+        apikey: apikey
+    }
+
+    return sendRequest(body);
+}
+
+//calls the api adn returns a json object of whatever the api returns
+function sendRequest(body) {
+  const request = new XMLHttpRequest;
+
+  request.onload = function () {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        return JSON.parse(this.responseText);
+      } else {
+        try {
+          const repsonse = JSON.parse(this.responseText)
+          return repsonse;
+        } catch {
+          //only returns text if the error cannot be made into a JSON object
+          return this.responseText;
+        }
+      }
+    }
+  }
+
+  request.open("POST", process.env.API_Location, true);
+  request.setRequestHeader("Content-Type","application/json");
+  request.send(JSON.stringify(body));
+}
