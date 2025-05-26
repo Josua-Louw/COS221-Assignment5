c7358a1d92e6aa3c75cc51f0233535ccf137a1eb
antonyvs05 <vanstratenantony@gmail.com>
2025-05-26 22:43:43 +0200

    created files

---
+++ front_end/storeManagement.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>CompareIt - StoreManage</title>
+    <link rel = "stylesheet" href = "css/storeManagement.css">
+    
+</head>
+<body>
+    
+    <?php
+require_once 'footer.php';
+?>
+</body>
+</html>
